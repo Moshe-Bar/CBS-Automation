fae226a475b11a1fad8aa058d88dda479b08ed50
Moshe-Bar <barmatzmesi@gmail.com>
2021-12-09 12:54:54 +0200

    09/12/2021 12:53

--- UI/PyWebIO/PyWebIO.py
+++ UI/PyWebIO/PyWebIO.py
@@ -13,15 +13,16 @@ class WebTest:
         self.pages_list = TestUtility.get_he_pages()
         self.data_share = (Queue(), Queue(), Queue())
 
+        self.chosen_pages = self.choose_pages()
+        self.start_test()
 
     def create_ui(self):
         self.progress_bar = put_processbar('bar')
 
     def choose_pages(self):
-
-        pages = list(map(lambda page: {'label': page.name, 'value': page.id, "selected": True}, pages_list))
+        pages = list(map(lambda page: {'label': page.name, 'value': page.id, "selected": True}, self.pages_list))
         chosen_pages = checkbox('Choose pages to test:', options=pages)
-        return list(filter(lambda x: x.id in chosen_pages, pages_list))
+        return list(filter(lambda x: x.id in chosen_pages, self.pages_list))
 
     def start_test(self):
         self.test_thread = threading.Thread(target=TestUtility.test, args=self.data_share)
@@ -32,24 +33,24 @@ class WebTest:
         self.observer_thread.join()
         self.test_thread.join()
 
-    def update_client_data(data: tuple):
+    def update_client_data(self,data:tuple):
         if data[0] == 'text':
             put_text(data[1])
         elif data[0] == 'link':
-            put_link(name=data[1], url=data[2])
+            put_link(name=data[1], url=data[2],new_window=True)
 
 
-    def update_client_bar(data):
+    def update_client_bar(self,data):
         pass
 
 
-    def observe_test(data: Queue, progress: Queue, end_flag: Queue, test_thread: threading.Thread):
+    def observe_test(self,data: Queue, progress: Queue, end_flag: Queue, test_thread: threading.Thread):
         while test_thread.is_alive() and end_flag.empty():
             if not data.empty():
-                update_client_data(data.get())
+                self.update_client_data(data.get())
 
             if not progress.empty():
-                update_client_bar(progress.get())
+                self.update_client_bar(progress.get())
 
         if not data.empty():
             put_text(data.get())
@@ -58,19 +59,8 @@ class WebTest:
         print('leaving observer')
 
 
-# def main():
-#     pages_for_test = choose_pages()
-#     shared_data = Queue()
-#     progress = Queue()
-#     end_flag = Queue()
-#     test_thread = threading.Thread(target=TestUtility.test,args=(shared_data,progress,end_flag,pages_for_test))
-#     observer_thread = threading.Thread(target=observe_test,args=(shared_data,progress,end_flag,test_thread))
-#     test_thread.start()
-#     observer_thread.start()
-#     observer_thread.join()
-#     test_thread.join()
-#     print('leaving main')
 
 
-# if __name__ == '__main__':
-#     main()
+
+if __name__ == '__main__':
+    test = WebTest()
